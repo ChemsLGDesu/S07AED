f8506aeb7e82eb574bca5c6016dfb852de9268a4
ChemsLGDesu <jamesgonelozano@gmail.com>
2026-05-04 23:34:21 -0500

    avance

--- Assets/Scripts/SEM07/EntityBase.cs
+++ Assets/Scripts/SEM07/EntityBase.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
 
-public class EntityBase : BaseStats
+public class EntityBase : MonoBehaviour
 {
-    public string namePlayer;
+    public BaseStats stats;
+    public string entityName;
+    public float id;
+    public float speed;
+
+    private void Awake()
+    {
+        if (stats != null)
+        {
+            entityName = stats.name;
+            id = stats.id;
+            speed = stats.speed;
+        }
+    }
 }
